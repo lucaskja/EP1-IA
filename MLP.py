42646dd612d72b78ece972f5bb13be48914d85df
GuiCamposxD <gui.xglpx@gmail.com>
2024-04-05 17:42:32 -0300

    fix(MLP): fix feedforward matrix multiplication

--- MLP.py
+++ MLP.py
@@ -1,27 +1,30 @@
 import numpy as np
 
 class Mlp:
-    def __init__(self, tamanho_camada_entrada, tamanho_camada_escondida, tamanho_camada_saida, vetor_entrada, vetor_esperado_saido):
-        self.tamanho_camada_entrada = tamanho_camada_entrada
+    def __init__(self, tamanho_camada_escondida, tamanho_camada_saida, vetor_entrada, vetor_esperado_saido):
+        self.tamanho_camada_entrada = len(vetor_entrada)
         self.tamanho_camada_escondida = tamanho_camada_escondida
         self.tamanho_camada_saida = tamanho_camada_saida
         self.vetor_entrada = vetor_entrada
         self.vetor_esperado_saido = vetor_esperado_saido
 
-        self.pesos_camada_escondida = np.random.uniform(-0.5, 0.51, [self.tamanho_camada_escondida, self.tamanho_camada_entrada])
-        self.pesos_camada_saida = np.random.uniform(-0.5, 0.51, [self.tamanho_camada_saida, self.tamanho_camada_escondida])
+        self.pesos_camada_entrada_para_escondida = np.random.uniform(-0.5, 0.51, [self.tamanho_camada_entrada, self.tamanho_camada_escondida])
+        self.pesos_camada_escondida_para_saida = np.random.uniform(-0.5, 0.51, [self.tamanho_camada_escondida, self.tamanho_camada_saida])
 
-        self.pesos_vies_camada_escondida = np.random.uniform(-0.5, 0.51, self.tamanho_camada_escondida)
-        self.pesos_vies_camada_saida = np.random.uniform(-0.5, 0.51, self.tamanho_camada_saida)
+        self.pesos_vies_camada_entrada_para_escondida = np.random.uniform(-0.5, 0.51, [1, self.tamanho_camada_escondida])
+        self.pesos_vies_camada_escondida_para_saida = np.random.uniform(-0.5, 0.51, [1, self.tamanho_camada_saida])
+
 
     def feedforward(self):
         self.saida_escondida = self.sigmoid(
-            np.dot(self.pesos_camada_escondida, self.vetor_entrada) + np.array(self.pesos_vies_camada_escondida).reshape(-1, 1)
+            np.dot(self.pesos_camada_entrada_para_escondida.T, self.vetor_entrada) + self.pesos_vies_camada_entrada_para_escondida.T
         )
+
         self.saida = self.sigmoid(
-            np.dot(self.pesos_camada_saida, self.saida_escondida) + np.array(self.pesos_vies_camada_saida).reshape(-1, 1)
+            np.dot(self.pesos_camada_escondida_para_saida.T, self.saida_escondida) + self.pesos_vies_camada_escondida_para_saida.T
         )
-        return np.array(self.saida)
+
+        return self.saida
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -36,5 +39,5 @@ for dados in arquivo_x:
     #transforma-se o array complexo (arquivo_x) em um array unidimensional, por meio da função flatten
     matriz_dados.append(dados.flatten())
 
-mlp = Mlp(len(matriz_dados), len(matriz_dados[0]), 26, matriz_dados, [])
+mlp = Mlp(4, 26, matriz_dados[0].reshape(-1, 1), matriz_resultado)
 print(mlp.feedforward())
