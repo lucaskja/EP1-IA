4b97e3766c4663b140d7fec6d6d4bb7b6a9ae0d9
GuiCamposxD <gui.xglpx@gmail.com>
2024-04-09 01:33:46 -0300

    fix(MLP): add bias to weigths matrix and change the feedforwards matrix multiplication

--- MLP.py
+++ MLP.py
@@ -1,32 +1,39 @@
 import numpy as np
+import os
 
 class Mlp:
     def __init__(self, tamanho_camada_escondida, tamanho_camada_saida, vetor_entrada, vetor_esperado_saido):
-        self.tamanho_camada_entrada = len(vetor_entrada)
+        self.vetor_entrada = np.insert(vetor_entrada, 0, 1)
+        self.vetor_esperado_saido = vetor_esperado_saido
         self.tamanho_camada_escondida = tamanho_camada_escondida
         self.tamanho_camada_saida = tamanho_camada_saida
-        self.vetor_entrada = vetor_entrada
-        self.vetor_esperado_saido = vetor_esperado_saido
+        self.tamanho_camada_entrada = len(self.vetor_entrada)
 
-        self.pesos_camada_entrada_para_escondida = np.load('pesos/pesos_camada_entrada_para_escondida.npy')
-        self.pesos_camada_escondida_para_saida = np.load('pesos/pesos_camada_escondida_para_saida.npy')
+        if os.path.exists('pesos/pesos_camada_entrada_para_escondida.npy') and os.path.exists('pesos/pesos_camada_escondida_para_saida.npy'):
+            self.pesos_camada_entrada_para_escondida = np.load('pesos/pesos_camada_entrada_para_escondida.npy')
+            self.pesos_camada_escondida_para_saida = np.load('pesos/pesos_camada_escondida_para_saida.npy')
+        else:
+            # As seis linhas abaixo servem para gerar randomicamente os pesos das camadas de entrada, escondida e saida num intervalo de -0,5 a 0,5.
+            self.pesos_camada_entrada_para_escondida = np.random.uniform(
+                -0.5, 0.51, [self.tamanho_camada_entrada, self.tamanho_camada_escondida]
+            )
+            self.pesos_camada_escondida_para_saida = np.random.uniform(
+                -0.5, 0.51, [self.tamanho_camada_escondida + 1, self.tamanho_camada_saida]
+            )
 
-        self.pesos_vies_camada_entrada_para_escondida = np.load('pesos/pesos_vies_camada_entrada_para_escondida.npy')
-        self.pesos_vies_camada_escondida_para_saida = np.load('pesos/pesos_vies_camada_escondida_para_saida.npy')
-
-        # As 4 linhas abaixo servem para gerar randomicamente os pesos das camadas de entrada, escondida e saida num intervalo de -0,5 a 0,5.
-        # self.pesos_camada_entrada_para_escondida = np.random.uniform(-0.5, 0.51, [self.tamanho_camada_entrada, self.tamanho_camada_escondida])
-        # self.pesos_camada_escondida_para_saida = np.random.uniform(-0.5, 0.51, [self.tamanho_camada_escondida, self.tamanho_camada_saida])
-        # self.pesos_vies_camada_entrada_para_escondida = np.random.uniform(-0.5, 0.51, [1, self.tamanho_camada_escondida])
-        # self.pesos_vies_camada_escondida_para_saida = np.random.uniform(-0.5, 0.51, [1, self.tamanho_camada_saida])
+            # As linhas duas abaixo servem para guardar em arquivos a matrix de pesos gerados para cada camada
+            np.save('pesos/pesos_camada_entrada_para_escondida.npy', self.pesos_camada_entrada_para_escondida)
+            np.save('pesos/pesos_camada_escondida_para_saida.npy', self.pesos_camada_escondida_para_saida)
 
     def feedforward(self):
-        self.saida_escondida = self.sigmoid(
-            np.dot(self.pesos_camada_entrada_para_escondida.T, self.vetor_entrada) + self.pesos_vies_camada_entrada_para_escondida.T
+        self.saida_escondida = np.insert(
+            self.sigmoid(np.dot(self.pesos_camada_entrada_para_escondida.T, self.vetor_entrada)),
+            0,
+            1,
         )
 
         self.saida = self.sigmoid(
-            np.dot(self.pesos_camada_escondida_para_saida.T, self.saida_escondida) + self.pesos_vies_camada_escondida_para_saida.T
+            np.dot(self.pesos_camada_escondida_para_saida.T, self.saida_escondida)
         )
 
         return self.saida
@@ -44,5 +51,5 @@ for dados in arquivo_x:
     # Transforma-se o array complexo (arquivo_x) em um array unidimensional, por meio da função flatten
     matriz_dados.append(dados.flatten())
 
-mlp = Mlp(4, 26, matriz_dados[0].reshape(-1, 1), matriz_resultado)
+mlp = Mlp(4, 26, matriz_dados[0], matriz_resultado)
 print(mlp.feedforward())
