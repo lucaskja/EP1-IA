03819340e4a22f62b2571a4afcd688c0a2b3fb2f
GuiCamposxD <gui.xglpx@gmail.com>
2024-04-14 16:52:01 -0300

    feat(Mlp): add backpropagation in mlp file considering any dataset

--- MLP.py
+++ MLP.py
@@ -2,13 +2,13 @@ import numpy as np
 import os
 
 class Mlp:
-    def __init__(self, tamanho_camada_escondida, tamanho_camada_saida, vetor_entrada, vetor_esperado_saido):
-        self.vetor_entrada = np.insert(vetor_entrada, 0, 1)
-        self.vetor_esperado_saido = vetor_esperado_saido
-        self.tamanho_camada_escondida = tamanho_camada_escondida
-        self.tamanho_camada_saida = tamanho_camada_saida
-        self.tamanho_camada_entrada = len(self.vetor_entrada)
+    def __init__(self, configuracao_mlp):
+        self.tamanho_camada_entrada = configuracao_mlp[0]
+        self.tamanho_camada_escondida = configuracao_mlp[1]
+        self.tamanho_camada_saida = configuracao_mlp[2]
+        self.taxa_apredizado = 0.5
 
+        # Verificamos se já existem o arquivos de pesos, caso não existam, cria-se novos com pesos aleatórios
         if os.path.exists('pesos/pesos_camada_entrada_para_escondida.npy') and os.path.exists('pesos/pesos_camada_escondida_para_saida.npy'):
             self.pesos_camada_entrada_para_escondida = np.load('pesos/pesos_camada_entrada_para_escondida.npy')
             self.pesos_camada_escondida_para_saida = np.load('pesos/pesos_camada_escondida_para_saida.npy')
@@ -25,19 +25,59 @@ class Mlp:
             np.save('pesos/pesos_camada_entrada_para_escondida.npy', self.pesos_camada_entrada_para_escondida)
             np.save('pesos/pesos_camada_escondida_para_saida.npy', self.pesos_camada_escondida_para_saida)
 
-    def feedforward(self):
+    def feedforward(self, letra):
+        self.soma_ponderada_camada_entrada_para_escondida = np.dot(
+            self.pesos_camada_entrada_para_escondida.T,
+            letra
+        )
         self.saida_escondida = np.insert(
-            self.sigmoid(np.dot(self.pesos_camada_entrada_para_escondida.T, self.vetor_entrada)),
+            self.sigmoid(self.soma_ponderada_camada_entrada_para_escondida),
             0,
             1,
         )
 
-        self.saida = self.sigmoid(
-            np.dot(self.pesos_camada_escondida_para_saida.T, self.saida_escondida)
+        self.soma_ponderada_camada_escondida_para_saida = np.dot(
+            self.pesos_camada_escondida_para_saida.T,
+            self.saida_escondida
         )
+        self.saida = self.sigmoid(self.soma_ponderada_camada_escondida_para_saida)
 
         return self.saida
+    
+    def backpropagation(self, letra, vetor_saida_esperada):
+        letra = np.insert(letra, 0, 1) # Coloca 1 na primeira posição da letra referente ao bias.
 
+        saida_rede = np.array(self.feedforward(letra))
+        
+        vetor_saida_menos_esperado = np.array(vetor_saida_esperada) - saida_rede # Realiza a operação: valor esperado na saída menos saida obtida
+
+        delta_saida = vetor_saida_menos_esperado * self.derivada_sigmoid(self.soma_ponderada_camada_escondida_para_saida) # Calcula o delta da camada de saída para atualizar os pesos da camada escondida para saída
+        self.termo_correcao_pesos_camada_escondida_para_saida = (
+            self.saida_escondida * np.array(delta_saida * self.taxa_apredizado).reshape(-1, 1)
+        ) # Calcula o termo de correção para cada peso da camada escondida para saída
+
+        delta_escondida = (
+            self.pesos_camada_escondida_para_saida[1:, :].dot(delta_saida) # O operador [1:, :] garante que a primeira linha da matriz de pesos é excluída, isso é importante, pois não consideramos os pesos do bias.
+            * self.derivada_sigmoid(self.soma_ponderada_camada_entrada_para_escondida)
+        )
+        self.termo_correcao_pesos_camada_entrada_para_escondida = (
+            letra * np.array(delta_escondida * self.taxa_apredizado).reshape(-1, 1)
+        )
+
+        self.pesos_camada_escondida_para_saida = (
+            self.pesos_camada_escondida_para_saida + self.termo_correcao_pesos_camada_escondida_para_saida.T
+        ) # Atualiza os pesos da camada escondida para saída. É importante tomar a transposta da matriz de pesos, pois voltamos as mesmas dimensões do vetor original.
+
+        self.pesos_camada_entrada_para_escondida = (
+            self.pesos_camada_entrada_para_escondida + self.termo_correcao_pesos_camada_entrada_para_escondida.T
+        ) # Atualiza os pesos da camada de entrada para escondida
+
+    def treinameto(self, epocas = 1, matriz_entrada = [], matriz_saida_esperada = []):
+        for epoca in range(epocas):
+            for (indice, letra) in enumerate(matriz_entrada):
+                self.backpropagation(letra, matriz_saida_esperada[indice])
+        
+            
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
@@ -48,11 +88,22 @@ arquivo_x = np.load('datasets/caracteres_completo/X.npy')
 arquivo_y = np.load('datasets/caracteres_completo/Y_classe.npy')
 
 matriz_dados = []
-matriz_resultado = []
 
 for dados in arquivo_x:
     # Transforma-se o array complexo (arquivo_x) em um array unidimensional, por meio da função flatten
-    matriz_dados.append(dados.flatten())
+    letra = dados.flatten()
+    letra[letra == -1] = 0
+    matriz_dados.append(letra)
 
-mlp = Mlp(4, 26, matriz_dados[0], matriz_resultado)
-print(mlp.feedforward())
+matriz_entrada_teste = [
+    matriz_dados[0],
+    matriz_dados[1]
+]
+
+matriz_saida_esperada= [
+    arquivo_y[0],
+    arquivo_y[1]
+]
+
+mlp = Mlp([120, 4, 26])
+mlp.treinameto(epocas = 500, matriz_entrada = matriz_entrada_teste, matriz_saida_esperada = matriz_saida_esperada)
--- MlpTeste.py
+++ MlpTeste.py
@@ -2,64 +2,64 @@ import numpy as np
 
 class MlpTeste:
     def __init__(self, tamanho_camada_escondida, tamanho_camada_saida, vetor_entrada, vetor_esperado_saida):
-      self.vetor_entrada = np.insert(vetor_entrada, 0, 1)
-      self.vetor_esperado_saida = vetor_esperado_saida
-      self.tamanho_camada_escondida = tamanho_camada_escondida
-      self.tamanho_camada_saida = tamanho_camada_saida
-      self.tamanho_camada_entrada = len(self.vetor_entrada)
-      self.taxa_apredizado = 0.5
+        self.vetor_entrada = np.insert(vetor_entrada, 0, 1)
+        self.vetor_esperado_saida = vetor_esperado_saida
+        self.tamanho_camada_escondida = tamanho_camada_escondida
+        self.tamanho_camada_saida = tamanho_camada_saida
+        self.tamanho_camada_entrada = len(self.vetor_entrada)
+        self.taxa_apredizado = 0.5
 
-      self.pesos_camada_entrada_para_escondida = np.array([
-        [-0.1, -0.1, 0.1],
-        [0.1, 0.1, -0.1], # Todo peso na posicao 0j é referente ao bias.
-        [-0.1, 0.1, -0.1],
-      ]) # 3x3, as colunas represetam a quantidade de neurônios da próxima camada e as linhas os neurônios da camada anterior
-      # Portanto, temos 4 neurônios na camada de entrada (3 neurônios + 1 do bias) e 2 neurônios na camada de saída.
+        self.pesos_camada_entrada_para_escondida = np.array([
+            [-0.1, -0.1, 0.1],
+            [0.1, 0.1, -0.1], # Todo peso na posicao 0j é referente ao bias.
+            [-0.1, 0.1, -0.1],
+        ]) # 3x3, as colunas represetam a quantidade de neurônios da próxima camada e as linhas os neurônios da camada anterior
+        # Portanto, temos 4 neurônios na camada de entrada (3 neurônios + 1 do bias) e 2 neurônios na camada de saída.
 
-      self.pesos_camada_escondida_para_saida = np.array([
-        [-0.1, 0.1],
-        [0.1, -0.1],
-        [0, 0.1],
-        [0.1, -0.1],
-      ]) # 3x2, as colunas represetam a quantidade de neurônios da próxima camada e as linhas os neurônios da camada anterior
+        self.pesos_camada_escondida_para_saida = np.array([
+            [-0.1, 0.1],
+            [0.1, -0.1],
+            [0, 0.1],
+            [0.1, -0.1],
+        ]) # 3x2, as colunas represetam a quantidade de neurônios da próxima camada e as linhas os neurônios da camada anterior
 
     def feedforward(self):
-      self.soma_ponderada_camada_entrada_para_escondida = np.dot(self.pesos_camada_entrada_para_escondida.T, self.vetor_entrada)
-      self.saida_escondida = np.insert(
+        self.soma_ponderada_camada_entrada_para_escondida = np.dot(self.pesos_camada_entrada_para_escondida.T, self.vetor_entrada)
+        self.saida_escondida = np.insert(
         self.sigmoid(self.soma_ponderada_camada_entrada_para_escondida),
         0,
         1,
-      )
+        )
 
-      self.soma_ponderada_camada_escondida_para_saida = np.dot(self.pesos_camada_escondida_para_saida.T, self.saida_escondida)
-      self.saida = self.sigmoid(self.soma_ponderada_camada_escondida_para_saida)
+        self.soma_ponderada_camada_escondida_para_saida = np.dot(self.pesos_camada_escondida_para_saida.T, self.saida_escondida)
+        self.saida = self.sigmoid(self.soma_ponderada_camada_escondida_para_saida)
 
-      return self.saida
+        return self.saida
     
     def backpropagation(self):
-      saida_rede = np.array(self.feedforward())
-      
-      vetor_saida_menos_esperado = np.array(self.vetor_esperado_saida) - saida_rede # Realiza a operação: valor esperado na saída menos saida obtida
+        saida_rede = np.array(self.feedforward())
+        
+        vetor_saida_menos_esperado = np.array(self.vetor_esperado_saida) - saida_rede # Realiza a operação: valor esperado na saída menos saida obtida
 
-      delta_saida = vetor_saida_menos_esperado * self.derivada_sigmoid(self.soma_ponderada_camada_escondida_para_saida) # Calcula o delta da camada de saída para atualizar os pesos da camada escondida para saída
-      self.termo_correcao_pesos_camada_escondida_para_saida = (
-        self.saida_escondida * np.array(delta_saida * self.taxa_apredizado).reshape(-1, 1)
-      ) # Calcula o termo de correção para cada peso da camada escondida para saída
+        delta_saida = vetor_saida_menos_esperado * self.derivada_sigmoid(self.soma_ponderada_camada_escondida_para_saida) # Calcula o delta da camada de saída para atualizar os pesos da camada escondida para saída
+        self.termo_correcao_pesos_camada_escondida_para_saida = (
+            self.saida_escondida * np.array(delta_saida * self.taxa_apredizado).reshape(-1, 1)
+        ) # Calcula o termo de correção para cada peso da camada escondida para saída
 
-      delta_escondida = (
-        self.pesos_camada_escondida_para_saida[1:, :].dot(delta_saida) # O operador [1:, :] garante que a primeira linha da matriz de pesos é excluída, isso é importante, pois não consideramos os pesos do bias.
-        * self.derivada_sigmoid(self.soma_ponderada_camada_entrada_para_escondida)
-      )
-      self.termo_correcao_pesos_camada_entrada_para_escondida = (
-        self.vetor_entrada * np.array(delta_escondida * self.taxa_apredizado).reshape(-1, 1)
-      )
+        delta_escondida = (
+            self.pesos_camada_escondida_para_saida[1:, :].dot(delta_saida) # O operador [1:, :] garante que a primeira linha da matriz de pesos é excluída, isso é importante, pois não consideramos os pesos do bias.
+            * self.derivada_sigmoid(self.soma_ponderada_camada_entrada_para_escondida)
+            )
+        self.termo_correcao_pesos_camada_entrada_para_escondida = (
+            self.vetor_entrada * np.array(delta_escondida * self.taxa_apredizado).reshape(-1, 1)
+        )
 
-      self.pesos_camada_escondida_para_saida = (
-        self.pesos_camada_escondida_para_saida + self.termo_correcao_pesos_camada_escondida_para_saida.T
-      ) # Atualiza os pesos da camada escondida para saída. É importante tomar a transposta da matriz de pesos, pois voltamos as mesmas dimensões do vetor original.
-      self.pesos_camada_entrada_para_escondida = (
-        self.pesos_camada_entrada_para_escondida + self.termo_correcao_pesos_camada_entrada_para_escondida.T
-      ) # Atualiza os pesos da camada de entrada para escondida
+        self.pesos_camada_escondida_para_saida = (
+            self.pesos_camada_escondida_para_saida + self.termo_correcao_pesos_camada_escondida_para_saida.T
+        ) # Atualiza os pesos da camada escondida para saída. É importante tomar a transposta da matriz de pesos, pois voltamos as mesmas dimensões do vetor original.
+        self.pesos_camada_entrada_para_escondida = (
+            self.pesos_camada_entrada_para_escondida + self.termo_correcao_pesos_camada_entrada_para_escondida.T
+        ) # Atualiza os pesos da camada de entrada para escondida
 
     def sigmoid(self, x):
       return 1 / (1 + np.exp(-x))
